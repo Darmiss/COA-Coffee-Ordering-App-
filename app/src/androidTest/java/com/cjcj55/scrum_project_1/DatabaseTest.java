c0497e3ed6456fda2699c9c76d9b333b72c1dad6
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2023-02-05 17:57:40 -0500

    Added objects that are dynamically created for Coffees, Toppings, and Flavors.

--- app/src/androidTest/java/com/cjcj55/scrum_project_1/DatabaseTest.java
+++ app/src/androidTest/java/com/cjcj55/scrum_project_1/DatabaseTest.java
@@ -22,67 +22,242 @@ public class DatabaseTest {
     @Test
     public void testDatabaseCreation() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        DatabaseHelper dbHelper = new DatabaseHelper(appContext);
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         assertTrue(db.isOpen());
     }
 
     @Test
-    public void testDataInsertion() {
+    public void testCoffeeInsertion() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        DatabaseHelper dbHelper = new DatabaseHelper(appContext);
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // Add a test tuple to the database
-        ContentValues values = new ContentValues();
-        values.put("name", "Coffee 1");
-        values.put("description", "Description 1");
-        values.put("price", "1.99");
-
-        // Insert tuple into database
-        long id = db.insert("coffee", null, values);
+        long id = dbHelper.insertCoffee("Coffee 2", "Description 2", 1.99);
 
         // Check if insertion was successful
         assertNotEquals(-1, id);
 
         // Query the database to verify data was inserted
-        Cursor cursor = db.query("coffee", null, "name=?", new String[]{ "Coffee 1" }, null, null, null);
+        Cursor cursor = db.query("coffee", null, "name=?", new String[]{ "Coffee 2" }, null, null, null);
         assertTrue(cursor.moveToFirst());
-        assertEquals("Coffee 1", cursor.getString(cursor.getColumnIndex("name")));
-        assertEquals("Description 1", cursor.getString(cursor.getColumnIndex("description")));
+        assertEquals("Coffee 2", cursor.getString(cursor.getColumnIndex("name")));
+        assertEquals("Description 2", cursor.getString(cursor.getColumnIndex("description")));
         assertEquals(1.99, cursor.getDouble(cursor.getColumnIndex("price")), 0.01);
 
         cursor.close();
+        db.close();
     }
 
     @Test
-    public void testDataRemoval() {
+    public void testCoffeeRemoval() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        DatabaseHelper dbHelper = new DatabaseHelper(appContext);
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // Add a test tuple to database
-        ContentValues values = new ContentValues();
-        values.put("name", "Test Coffee");
-        values.put("description", "A test coffee");
-        values.put("price", "1.99");
-        long id = db.insert("coffee", null, values);
+        long id = dbHelper.insertCoffee("Deletion Coffee", "Desc", 4.99);
 
         // Verify tuple was added to database
-        Cursor cursor = db.query("coffee", null, null, null, null, null, null);
         assertNotEquals(-1, id);
-        cursor.close();
 
         // Remove the test tuple
-        int rowsDeleted = db.delete("coffee", "name=?", new String[]{ "Test Coffee" });
+        int rowsDeleted = dbHelper.deleteCoffee("Deletion Coffee");
 
         // Check if removal was successful
         assertEquals(1, rowsDeleted);
 
         // Query the database to verify data was removed
-        cursor = db.query("coffee", null, "name=?", new String[]{ "Test Coffee" }, null, null, null);
+        Cursor cursor = db.query("coffee", null, "name=?", new String[]{ "Deletion Coffee" }, null, null, null);
         assertFalse(cursor.moveToFirst());
+        cursor.close();
+
+        db.close();
+    }
+
+    @Test
+    public void testToppingInsertion() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to the database
+        long id = dbHelper.insertTopping("Topping 2", "Description 2", 0.59);
+
+        // Check if insertion was successful
+        assertNotEquals(-1, id);
+
+        // Query the database to verify data was inserted
+        Cursor cursor = db.query("toppings", null, "name=?", new String[]{ "Topping 2" }, null, null, null);
+        assertTrue(cursor.moveToFirst());
+        assertEquals("Topping 2", cursor.getString(cursor.getColumnIndex("name")));
+        assertEquals("Description 2", cursor.getString(cursor.getColumnIndex("description")));
+        assertEquals(0.59, cursor.getDouble(cursor.getColumnIndex("price")), 0.01);
 
         cursor.close();
+        db.close();
+    }
+
+    @Test
+    public void testToppingRemoval() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to database
+        long id = dbHelper.insertTopping("Deletion Topping", "Desc", 0.42);
+
+        // Verify tuple was added to database
+        assertNotEquals(-1, id);
+
+        // Remove the test tuple
+        int rowsDeleted = dbHelper.deleteTopping("Deletion Topping");
+
+        // Check if removal was successful
+        assertEquals(1, rowsDeleted);
+
+        // Query the database to verify data was removed
+        Cursor cursor = db.query("toppings", null, "name=?", new String[]{ "Deletion Topping" }, null, null, null);
+        assertFalse(cursor.moveToFirst());
+        cursor.close();
+
+        db.close();
+    }
+
+    @Test
+    public void testFlavorInsertion() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to the database
+        long id = dbHelper.insertFlavor("Flavor 2", "Description 2", 0.99);
+
+        // Check if insertion was successful
+        assertNotEquals(-1, id);
+
+        // Query the database to verify data was inserted
+        Cursor cursor = db.query("flavors", null, "name=?", new String[]{ "Flavor 2" }, null, null, null);
+        assertTrue(cursor.moveToFirst());
+        assertEquals("Flavor 2", cursor.getString(cursor.getColumnIndex("name")));
+        assertEquals("Description 2", cursor.getString(cursor.getColumnIndex("description")));
+        assertEquals(0.99, cursor.getDouble(cursor.getColumnIndex("price")), 0.01);
+
+        cursor.close();
+        db.close();
+    }
+
+    @Test
+    public void testFlavorRemoval() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to database
+        long id = dbHelper.insertCoffee("Deletion Flavor", "Desc", 0.99);
+
+        // Verify tuple was added to database
+        assertNotEquals(-1, id);
+
+        // Remove the test tuple
+        int rowsDeleted = dbHelper.deleteCoffee("Deletion Flavor");
+
+        // Check if removal was successful
+        assertEquals(1, rowsDeleted);
+
+        // Query the database to verify data was removed
+        Cursor cursor = db.query("flavors", null, "name=?", new String[]{ "Deletion Flavor" }, null, null, null);
+        assertFalse(cursor.moveToFirst());
+        cursor.close();
+
+        db.close();
+    }
+
+    @Test
+    public void testUserInsertion() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to the database
+        long id = dbHelper.insertUser("username1", "password1", "email1@gmail.com", "FirstName1", "LastName1");
+
+        // Check if insertion was successful
+        assertNotEquals(-1, id);
+
+        // Query the database to verify data was inserted
+        Cursor cursor = db.query("users", null, "username=?", new String[]{ "username1" }, null, null, null);
+        assertTrue(cursor.moveToFirst());
+        assertEquals("username1", cursor.getString(cursor.getColumnIndex("username")));
+        assertEquals("password1", cursor.getString(cursor.getColumnIndex("password")));
+        assertEquals("email1@gmail.com", cursor.getString(cursor.getColumnIndex("email")));
+        assertEquals("FirstName1", cursor.getString(cursor.getColumnIndex("firstName")));
+        assertEquals("LastName1", cursor.getString(cursor.getColumnIndex("lastName")));
+
+        cursor.close();
+        db.close();
+    }
+
+    @Test
+    public void testUserLoginPositive() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to the database
+        long id = dbHelper.insertUser("username2", "password2", "email2@gmail.com", "FirstName2", "LastName2");
+
+        // Check if insertion was successful
+        assertNotEquals(-1, id);
+
+        // Query the database to verify data was inserted
+        Cursor cursor = db.query("users", null, "username=?", new String[]{ "username2" }, null, null, null);
+        assertTrue(cursor.moveToFirst());
+        assertEquals("username2", cursor.getString(cursor.getColumnIndex("username")));
+        assertEquals("password2", cursor.getString(cursor.getColumnIndex("password")));
+        assertEquals("email2@gmail.com", cursor.getString(cursor.getColumnIndex("email")));
+        assertEquals("FirstName2", cursor.getString(cursor.getColumnIndex("firstName")));
+        assertEquals("LastName2", cursor.getString(cursor.getColumnIndex("lastName")));
+        cursor.close();
+
+        // Remove the test tuple
+        boolean loggedIn = dbHelper.userLogin("email2@gmail.com", "password2");
+
+        // Check if user was successfully logged in
+        assertTrue(loggedIn);
+
+        db.close();
+    }
+
+    @Test
+    public void testUserLoginNegative() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper dbHelper = DatabaseHelper.getInstance(appContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Add a test tuple to the database
+        long id = dbHelper.insertUser("username3", "password3", "email3@gmail.com", "FirstName3", "LastName3");
+
+        // Check if insertion was successful
+        assertNotEquals(-1, id);
+
+        // Query the database to verify data was inserted
+        Cursor cursor = db.query("users", null, "username=?", new String[]{ "username3" }, null, null, null);
+        assertTrue(cursor.moveToFirst());
+        assertEquals("username3", cursor.getString(cursor.getColumnIndex("username")));
+        assertEquals("password3", cursor.getString(cursor.getColumnIndex("password")));
+        assertEquals("email3@gmail.com", cursor.getString(cursor.getColumnIndex("email")));
+        assertEquals("FirstName3", cursor.getString(cursor.getColumnIndex("firstName")));
+        assertEquals("LastName3", cursor.getString(cursor.getColumnIndex("lastName")));
+        cursor.close();
+
+        // Remove the test tuple
+        boolean loggedIn = dbHelper.userLogin("email3@gmail.com", "password2");
+
+        // Check if user was successfully logged in
+        assertFalse(loggedIn);
+
+        db.close();
     }
 }
--- app/src/main/java/com/cjcj55/scrum_project_1/objects/Coffee.java
+++ app/src/main/java/com/cjcj55/scrum_project_1/objects/Coffee.java
@@ -1,4 +1,87 @@
 package com.cjcj55.scrum_project_1.objects;
 
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
 public class Coffee {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private List<Topping> toppings;
+    private List<Flavor> flavors;
+
+    public Coffee(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
+
+    public List<Flavor> getFlavors() {
+        return flavors;
+    }
+
+    public void setFlavors(List<Flavor> flavors) {
+        this.flavors = flavors;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        String coffeeStr = getId() + ", " + getName() + ", " + getDescription() + ", $" + getPrice();
+//        if (!this.toppings.isEmpty()) {
+//            for (int i = 0; i < toppings.size(); i++)
+//            {
+//                coffeeStr += "\t" + toppings.get(i).toString();
+//            }
+//        }
+//        if (!this.flavors.isEmpty()) {
+//            for (int i = 0; i < flavors.size(); i++)
+//            {
+//                coffeeStr += "\t" + flavors.get(i).toString();
+//            }
+//        }
+        return coffeeStr;
+    }
 }
