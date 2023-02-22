2731633e56d280243f6fc063c59ac73b74e5d4b9
natha <nrs79@pitt.edu>
2023-02-22 12:16:44 -0500

    Takes out "Back to menu" function from WorkerOrderScreen and instead logs user out

--- app/src/main/java/com/cjcj55/scrum_project_1/WorkerOrderScreen.java
+++ app/src/main/java/com/cjcj55/scrum_project_1/WorkerOrderScreen.java
@@ -1,5 +1,10 @@
 package com.cjcj55.scrum_project_1;
 
+import static com.cjcj55.scrum_project_1.LoginScreen.setAccountCreationPopup;
+import static com.cjcj55.scrum_project_1.LoginScreen.setLoggedOutPopup;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -7,12 +12,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cjcj55.scrum_project_1.databinding.WorkescreenuiBinding;
 import com.cjcj55.scrum_project_1.db.SQLiteDatabaseHelper;
 import com.cjcj55.scrum_project_1.objects.UserCart;
@@ -188,8 +200,35 @@ public class WorkerOrderScreen extends Fragment {
         binding.BacktoMenuWorkerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(WorkerOrderScreen.this)
-                        .navigate(R.id.action_WorkerOrderScreen_to_OrderScreen);
+                Context context = getContext();
+
+                MainActivity.userCart = new UserCart();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        "http://" + MainActivity.LOCAL_IP + "/logout.php",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                setLoggedOutPopup(true); //makes it so when going back to login screen, logged out popup popups
+                                setAccountCreationPopup(false); //disables account creation popup
+
+                                SharedPreferences sharedPreferences1 = getContext().getSharedPreferences("myAppPrefs", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences1.edit();
+                                editor.clear();
+                                editor.apply();
+
+                                NavHostFragment.findNavController(WorkerOrderScreen.this)
+                                        .navigate(R.id.action_WorkerOrderScreen_to_LoginScreen);
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "error:" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                RequestQueue queue = Volley.newRequestQueue(context);
+                queue.add(stringRequest);
             }
         });
 
